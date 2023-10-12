e834e7fd665369008615c1f0e6c2364412ba1ebe
shehryar60 <shehryar60@gmail.com>
2023-10-13 01:32:39 +0500

    uploading

---
+++ part01-Part01_23.SimpleCalculator/src/main/java/SimpleCalculator.java
@@ -0,0 +1,30 @@
+
+import java.util.Scanner;
+
+public class SimpleCalculator {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Write your program here
+        System.out.println("Give the first number: ");
+        int a=Integer.valueOf(scanner.nextLine());
+        System.out.println("Give the second number: ");
+        int b=Integer.valueOf(scanner.nextLine());
+       
+        int c=a+b;
+        System.out.println(a+" + "+b+" = "+c);
+        
+         c=a-b;
+        System.out.println(a+" - "+b+" = "+c);
+        
+         c=a*b;
+        System.out.println(a+" * "+b+" = "+c);
+        
+        double d=(double)a/b;
+        System.out.println(a+" / "+b+" = "+d);
+        
+
+        
+    }
+}
