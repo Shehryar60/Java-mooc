58f0cb8a05addd0c7501412d7d04fa6ad440b919
shehryar60 <shehryar60@gmail.com>
2023-11-27 22:58:13 +0500

    all

---
+++ part03-Part03_05.LastInList/src/main/java/LastInList.java
@@ -0,0 +1,25 @@
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class LastInList {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = 0;
+
+        ArrayList<String> list = new ArrayList<>();
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("")) {
+                break;
+            }
+
+            list.add(input);
+            a++;
+        }
+        int index = a - 1;
+        System.out.println(list.get(index));
+
+    }
+}
